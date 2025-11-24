d9befec1678c0706f09d022287da0d9ce43fe01c
DyerMandujano <dyer0206@gmail.com>
2025-11-23 19:11:38 -0500

    CRUD completo de MARCAS

--- Controllers/MarcasController.cs
+++ Controllers/MarcasController.cs
@@ -2,6 +2,7 @@
 using Microsoft.Data.SqlClient;
 using Microsoft.EntityFrameworkCore;
 using Pry_Solu_SalonSPA.Db;
+using Pry_Solu_SalonSPA.Models;
 using System.Linq;
 
 namespace Pry_Solu_SalonSPA.Controllers
@@ -13,9 +14,108 @@ namespace Pry_Solu_SalonSPA.Controllers
         {
             _context = context;
         }
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
-            return View();
+            var marcas = await _context.Marcas
+                .FromSqlRaw("EXEC SP_ListarMarcas")
+                .ToListAsync();
+
+            return View(marcas);
         }
+
+        public IActionResult Crear()
+        {
+            return View("_CrearMarca", new Marca());
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Crear(Marca marca)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View("_CrearMarca", marca);
+            }
+
+            var paramNom = new SqlParameter("@nom_marca", marca.NomMarca);
+
+            await _context.Database.ExecuteSqlRawAsync(
+                "EXEC SP_InsertMarca @nom_marca",
+                paramNom
+            );
+
+            return RedirectToAction(nameof(Index));
+        }
+
+        public async Task<IActionResult> Editar(int id)
+        {
+            var marca = await _context.Marcas
+                .FromSqlRaw("SELECT * FROM Marca WHERE Id_Marca = {0}", id)
+                .FirstOrDefaultAsync();
+
+            if (marca == null)
+                return NotFound();
+
+            ViewBag.IdMarca = id;
+
+            return View("_EditarMarca", marca);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Editar(int id, Marca marca)
+        {
+            if (!ModelState.IsValid)
+            {
+                ViewBag.IdMarca = id;
+                return View("_EditarMarca", marca);
+            }
+
+            var paramId = new SqlParameter("@id_marca", id);
+            var paramNom = new SqlParameter("@nom_marca", marca.NomMarca);
+            var paramEstado = new SqlParameter("@estado", marca.Estado);
+
+            await _context.Database.ExecuteSqlRawAsync(
+                "EXEC SP_UpdateMarca @id_marca, @nom_marca, @estado",
+                paramId, paramNom, paramEstado
+            );
+
+            return RedirectToAction(nameof(Index));
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> CambiarEstado(int id)
+        {
+
+            var marca = await _context.Marcas
+                .FromSqlRaw("SELECT * FROM Marca WHERE Id_Marca = {0}", id)
+                .FirstOrDefaultAsync();
+
+            if (marca == null)
+                return NotFound();
+
+
+            if (marca.Estado == 1)
+            {
+                var paramId = new SqlParameter("@id_marca", id);
+                await _context.Database.ExecuteSqlRawAsync(
+                    "EXEC SP_DeleteMarca @id_marca",
+                    paramId
+                );
+            }
+            else
+            {
+                var paramId = new SqlParameter("@id_marca", id);
+
+                await _context.Database.ExecuteSqlRawAsync(
+                    "UPDATE Marca SET Estado = 1 WHERE Id_Marca = @id_marca",
+                    paramId
+                );
+            }
+
+            return RedirectToAction(nameof(Index));
+        }
+
     }
 }
